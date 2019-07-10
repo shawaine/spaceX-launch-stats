581169393035b50f0c26212110b4ee03463602fa
zeirwayne <zeirwayne@github.com>
2019-07-10 11:42:35 +0800

    second deploy

---
+++ public/build/precache-manifest.1c6eb555f094d303359e9c03364c9371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0712e1a986d978ccb4430d957a6e34b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4c720218e87baa83fd",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "b8a1e940355a8216756c",
+    "url": "/static/js/2.76ef88bc.chunk.js"
+  },
+  {
+    "revision": "be4c720218e87baa83fd",
+    "url": "/static/js/main.b781648b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2d5891c2d3a63822aec77b9cedeb889",
+    "url": "/static/media/spaceX.b2d5891c.jpg"
+  }
+]);
